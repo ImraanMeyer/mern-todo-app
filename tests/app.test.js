4f0e2a44b3b22508caed150c3b11aa9e36c0cee7
ImraanMeyer <imraan.meyer97@gmail.com>
2020-04-27 15:53:23 +0200

    removed env from travis

--- tests/app.test.js
+++ tests/app.test.js
@@ -5,7 +5,6 @@ const app = require('../app');
 
 describe('Todos', function() {
     it('should return unauthorized error', function(done) {
-
         request(app)
             .get('/api/todos')
             .expect(401, done())
